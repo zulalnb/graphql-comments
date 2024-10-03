b3414619e32ccdbe34e6f502060a0ce4cd6837be
Zülal Nebin <zllnbn1997@gmail.com>
2024-10-03 20:51:13 +0300

    merge resolver and type def files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import { createServer } from "node:http";
 import { createSchema, createYoga } from "graphql-yoga";
 import pubSub from "./pubSub.js";
 
-import typeDefs from "./graphql/type-defs/index.js";
+// import typeDefs from "./graphql/type-defs/index.js";
 
-import resolvers from "./graphql/resolvers/index.js";
+// import resolvers from "./graphql/resolvers/index.js";
 import db from "./data.js";
+import schema from "./graphql/schema.js";
 
 const yoga = createYoga({
-  schema: createSchema({
-    resolvers,
-    typeDefs,
-  }),
+  schema,
   logging: true,
   context: { pubSub, db },
 });
